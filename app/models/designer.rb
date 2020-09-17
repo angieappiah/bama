dcf505251ebcbf7b05082e9ab54cbe14ccab14cb
angieappiah <alegnalawn2@yahoo.com>
2020-09-17 11:40:55 -0700

    bama

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -2,8 +2,8 @@ class Designer < ApplicationRecord
     has_many :styles
     has_many :pieces, through: :style
 
-    validates :username, presence :true
+    validates :username, presence: true
     validates :password, :confirmation => true
-    validates :email, presence :true, uniqueness: true
-    validates :phone, presence :true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :phone, presence: true, uniqueness: true
 end
