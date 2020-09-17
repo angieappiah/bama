7c3cd57e37bfa21d3f4302286a75abc654bc2540
angieappiah <alegnalawn2@yahoo.com>
2020-09-17 11:37:07 -0700

    bama

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,38 @@ class SessionsController < ApplicationController
     end
 
     def create
+
+        if auth
+            user = User.find_or_create_by(uid: auth['uid']) do |u|
+              u.username = auth['info']['name']
+              u.email = auth['info']['email']
+              u.password = auth['uid']
+            end
+            session[:user_id] = user.id
+            redirect_to users_path(user)
+      
+           else
+            @user = User.find_by(email: params[:user][:email])
+           if @user && @user.authenticate(params[:user][:password])
+             session[:user_id] = @user.id
+             redirect_to users_path(@user)
+           else
+            render :new
+           end
+        end
         
     end
 
     def destroy
+      session.clear
+      redirect_to '/'
+    end
+
+
+    private
+
+    def auth
+      request.env['omniauth.auth']
     end
 
 
