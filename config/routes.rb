ea342573d26acaf20268114b20fe20473c5a1a7e
angieappiah <alegnalawn2@yahoo.com>
2020-09-17 12:35:20 -0700

    bama

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   resources :pieces
   resources :designers
 
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
+  get  '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
+
+  get  '/auth/facebook/callback' => 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
